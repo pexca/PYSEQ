16c9b29c5a4829ea3f9bcf76e30602fdc11c1ab0
pexca <pexca@rambler.ru>
2020-03-18 04:29:22 +0300

    ew pages added, base template created

--- mY_python code/learning_log/urls.py
+++ mY_python code/learning_log/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'', include('learning_logs.urls', namespace='learning_logs'))
+    path('', include('learning_logs.urls')),
 ]
+
+
--- mY_python code/learning_logs/urls.py
+++ mY_python code/learning_logs/urls.py
@@ -1,6 +1,10 @@
 """Определяет схемы URL для learning_logs."""
-
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
-urlpatterns = [url(r'^$', views.index, name='index')]
+app_name = 'learning_logs'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics')
+]
+
