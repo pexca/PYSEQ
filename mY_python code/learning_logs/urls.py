cccf0d36577a45844778cf4df8dffbb4b16992e1
pexca <pexca@rambler.ru>
2020-03-19 03:34:57 +0300

    changed entryForm
    added functionality

--- mY_python code/learning_logs/urls.py
+++ mY_python code/learning_logs/urls.py
@@ -5,6 +5,11 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry')
 ]
 
